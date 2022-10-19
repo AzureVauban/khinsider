ce1db26c610ff0f40bf3c0e9ed277b20ce82f43e
AzureVauban <haloringman@outlook.com>
2022-10-19 08:17:23 +0000

    Oct 19, 2022, 3:17 AM

--- import khinsider - python script.py
+++ import khinsider - python script.py
@@ -1,6 +1,6 @@
 import khinsider
 print('\x1B[31mProgram has begun Running, Please wait until the end message in appears\x1B[37m')
-khinsider.download('terraria-calamity-the-tale-of-a-cruel-world')
+khinsider.download('new-super-mario-brothers')
 print('\x1B[32mFinished Downloading Album, please move files into a folder to avoid disorganization within source folder\x1B[0m')
 # this is a test script 
 #* https://downloads.khinsider.com/game-soundtracks/album/super-smash-bros.-anthology-vol.-01-super-smash-bros
